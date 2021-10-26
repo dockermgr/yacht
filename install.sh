92bd409eb59f16f771696b581c14915c31b9eee0
casjay <casjay@yahoo.com>
2021-10-25 21:42:51 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -173,7 +173,7 @@ fi
 # run post install scripts
 run_postinst() {
   dockermgr_run_post
-  grep -sq "$APPNAME.local" /etc/hosts || echo "$SERVER_LISTEN     $APPNAME.local" | sudo tee -a /etc/hosts &>/dev/null
+  grep -sq "$APPNAME.local" /etc/hosts || { [[ -n "$SERVER_PORT_INT" ]] && echo "$SERVER_LISTEN     $APPNAME.local" | sudo tee -a /etc/hosts &>/dev/null; }
 }
 #
 execute "run_postinst" "Running post install scripts"
